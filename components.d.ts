a325c645ef463b307a4cb6d1e3fe5038e4570b47
liusaisai <56034541+liusaisaia@users.noreply.github.com>
2023-05-26 18:32:39 +0800

    feat(增加表头): 新增

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,11 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElAvatar: typeof import('element-plus/es')['ElAvatar']
+    ElBadge: typeof import('element-plus/es')['ElBadge']
+    ElOption: typeof import('element-plus/es')['ElOption']
+    ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElSwitch: typeof import('element-plus/es')['ElSwitch']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
